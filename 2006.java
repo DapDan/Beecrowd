8d8616760d13b1dfb6a18e9efdedab976dcc9c3a
DapDan <daniloalexandre745@gmail.com>
2025-12-12 13:06:42 -0300

    2006

---
+++ 2006.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            int value = sc.nextInt();
+            if (value == n) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+	}
+}
--- 2006.js
+++ 2006.js
@@ -1,7 +1,16 @@
 const fs = require("fs");
-const data = fs.readFileSync(0, "utf8").trim().split("/\s+/").map(Number);    
+const data = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number);
 
 let idx = 0;
 const n = data[idx++];
 
-console.log(n);
+let count = 0;
+
+for (let i = 0; i < data.length - 1; i++) {
+    const value = data[idx++];
+    if (value === n) {
+        count++;
+    }
+}
+
+console.log(count);
